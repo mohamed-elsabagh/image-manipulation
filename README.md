4c9e397633559ea9ddc059f356c3b6765fd31603
Mohamed El-Sabagh <mohamed.elsabagh88@gmail.com>
2017-02-18 02:46:07 +0200

    Added build script and README.md

---
+++ README.md
@@ -0,0 +1,18 @@
+# Image Manipulation #
+This repository contains a node js script which help smart phone developers to create images for their projects in different sizes.
+
+### Prerequisites ###
+---
+Before starting you must install node on your machine.
+
+This procedures was tested over an ubuntu machine
+
+* ###### Install required packages & dependencies ######
+Go to the project root folder and run the following command
+```./build.sh install```
+
+* ###### Clean any old build ######
+To clean and remove any old build run ```./build.sh clean```
+
+### Launch the script ###
+To launch the script run ```./build.sh run```
---
+++ build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+PROJECT_HOME=$(pwd)
+
+function help {
+	echo "Choose one of the following: {install|clean}"
+	exit 1
+}
+
+function install {
+	echo "Installing required packages"
+	npm install &&
+	echo "Installing required dependencies & tools"
+  sudo apt-get install graphicsmagick &&
+	sudo apt-get install imagemagick
+}
+
+function clean {
+	echo "Cleaning the directory"
+  cd $PROJECT_HOME
+	rm -r -f *.png &&
+  rm -r -f *.jpg &&
+  rm -r -f *.jpeg &&
+  rm -r -f *.gif &&
+  rm -r -f *.svg &&
+  rm -r -f drawable-xxxhdpi &&
+  rm -r -f drawable-xxhdpi &&
+  rm -r -f drawable-xhdpi &&
+  rm -r -f drawable-hdpi &&
+  rm -r -f drawable-mdpi
+}
+
+function run {
+	echo "Launching the script"
+  cd $PROJECT_HOME &&
+	./image.js
+}
+
+if [ $# -eq 0 ]
+then
+	help
+fi
+
+for cmd in $@
+do
+
+case "$cmd" in
+
+	install)
+		install
+	;;
+
+	clean)
+		clean
+	;;
+
+	run)
+		run
+	;;
+
+	*)
+		help
+	;;
+esac
+
+done
